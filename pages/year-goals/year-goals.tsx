1f2c9a4cdaa6cbf0f03d029427121b6e98ddcc90
Julia Malinina <malinina.artwork@gmail.com>
2022-11-18 23:30:10 +0100

    Install and configure i18next

--- pages/year-goals/year-goals.tsx
+++ pages/year-goals/year-goals.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 import { Typography } from 'antd';
 
 const YearGoalsPage = () => {
+  const { t } = useTranslation();
   const { Title } = Typography;
+  const pageTitle = t('year_goals');
 
   return (
     <>
-      <Head><title>Year Goals</title></Head>
-      <Title>Year Goals</Title>
-      <Link href="/">Back to home page</Link>
+      <Head><title>{pageTitle}</title></Head>
+      <Title>{pageTitle}</Title>
+      <Link href="/">{t('back_to_home_page')}</Link>
     </>
   );
 };
